16a433ea02b0e298c7ff0c5be3f04e3e4ef10239
Hendrick_Samuel <samuel@sparkle.tech>
2025-02-04 22:42:42 +0100

    Added theme check-mark

--- src/components/ThemePicker.tsx
+++ src/components/ThemePicker.tsx
@@ -1,12 +1,17 @@
-"use client"
+"use client";
 
-import { useTheme } from "./ThemeProvider"
-import { Button } from "@/components/ui/button"
-import { Palette } from "lucide-react"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { useTheme } from "./ThemeProvider";
+import { Button } from "@/components/ui/button";
+import { Check, Palette } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function ThemePicker() {
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
   return (
     <DropdownMenu>
@@ -20,17 +25,31 @@ export default function ThemePicker() {
         <DropdownMenuItem onClick={() => setTheme("red")}>
           <div className="w-4 h-4 rounded-full bg-red-500 mr-2" />
           Red
+          {theme === "red" && (
+            <span className="ml-auto">
+              <Check className="w-4 h-4" />
+            </span>
+          )}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("blue")}>
           <div className="w-4 h-4 rounded-full bg-blue-500 mr-2" />
           Blue
+          {theme === "blue" && (
+            <span className="ml-auto">
+              <Check className="w-4 h-4" />
+            </span>
+          )}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("sakura")}>
           <div className="w-4 h-4 rounded-full bg-pink-300 mr-2" />
           Sakura
+          {theme === "sakura" && (
+            <span className="ml-auto">
+              <Check className="w-4 h-4" />
+            </span>
+          )}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
-
